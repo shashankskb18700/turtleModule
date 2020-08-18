89d6cc319e41b0f62a5ac06a9841ee700145344f
shashank <69819155+shashankskb18700@users.noreply.github.com>
2020-08-19 00:59:23 +0530

    Create square.py

---
+++ square.py
@@ -0,0 +1,25 @@
+import turtle
+bob=turtle.Turtle()
+#print(bob)
+#turtle.mainloop()
+#bob.fd(100)
+#bob.lt(90)
+#bob.fd(100)
+#bob.lt(90)
+#bob.fd(100)
+#bob.lt(270)
+#bob.fd(100)
+#bob.lt(270)
+#bob.fd(100)
+for  i in range(3):
+	bob.fd(100)
+	bob.lt(90)
+for t in range(1):
+	
+	bob.lt(180)
+	bob.fd(100)
+	bob.lt(270)
+	bob.fd(100)
+  
+  #either use manualy typing or use iterator result will be same
+  
